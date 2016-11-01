8edcd58f05b255c2a348f76f0cf61b3f4f2bd98a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-11-01 13:43:12 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var run = true;
+            Console.CancelKeyPress += (sender, args) => run = false;
+
+            while (run)
+            {
+                Console.WriteLine("Hello World!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
